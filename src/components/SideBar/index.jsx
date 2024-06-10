28b25acf487c8d94109b45c954786d7ecfd4ec58
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 21:27:40 -0600

    replace li tags for NavItem elements and add props

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,16 +1,33 @@
 import styled from "styled-components";
+import NavItem from "./NavItem";
+
+const StyledList = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`;
 
 function SideBar() {
     return (
         <aside>
             <nav>
-                <ul>
-                    <li>Home</li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+                <StyledList>
+                    <NavItem activeIcon='iconos/home-activo.png' innactiveIcon='iconos/home-inactivo.png' active >
+                        Home
+                    </NavItem>
+                    <NavItem activeIcon='iconos/mas-vistas-activo.png' innactiveIcon='iconos/mas-vistas-inactivo.png' >
+                        Most Viewed
+                    </NavItem>
+                    <NavItem activeIcon='iconos/me-gusta-activo.png' innactiveIcon='iconos/me-gusta-inactivo.png' >
+                        Most Liked
+                    </NavItem>
+                    <NavItem activeIcon='iconos/nuevas-activo.png' innactiveIcon='iconos/nuevas-inactivo.png' >
+                        Recent
+                    </NavItem>
+                    <NavItem activeIcon='iconos/sorprendeme-activo.png' innactiveIcon='iconos/sorprendeme-inactivo.png' >
+                        Surprise Me
+                    </NavItem>
+                </StyledList>
             </nav>
         </aside>
     );
