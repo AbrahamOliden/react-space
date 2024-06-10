20292c396de67a830df766a83f88063bf77e23f3
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 20:36:21 -0600

    delete InputField component import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components"
 import GlobalStyle from "./components/GlobalStyles"
 import Header from "./components/Header"
-import InputField from "./components/InputField"
 
 const GradientBackground = styled.div`
 background: linear-gradient(175deg, 
@@ -16,7 +15,6 @@ function App() {
       <GradientBackground>
         <GlobalStyle />
         <Header />
-        <InputField />
       </GradientBackground>
     </>
   )
