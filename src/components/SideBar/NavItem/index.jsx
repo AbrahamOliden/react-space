ef56ea62b964f685853f878fd4ad4cdd18adfb63
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 21:28:46 -0600

    create StyledElement and use conditional based on props

--- src/components/SideBar/NavItem/index.jsx
+++ src/components/SideBar/NavItem/index.jsx
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
+const StyledElement = styled.li`
+    display: flex;
+    align-items: center;
+    font-size: 1.5rem;
+    margin-bottom: 30px;
+    gap: 1rem;
+    color: ${props => props.$active ? '#7B78E5' : '#D9D9D9'};
+`;
+
 function NavItem({children, activeIcon, innactiveIcon, active=false}) {
-    return <>
+    return <StyledElement $active={active} >
     <img src={active ? activeIcon : innactiveIcon} alt="Icon" />
     {children}
-    </>
+    </StyledElement>
 };
 
 export default NavItem;
