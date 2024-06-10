0e4b486795d190ab4e6974c3868f7dc93a9f954e
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 20:34:56 -0600

    finish styling input and icon

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -13,7 +13,7 @@ const StyledInput = styled.input`
     border: 2px solid #C98CF1;
     background: transparent;
     box-sizing: border-box;
-    width: 50%;
+    min-width: 500px;
     position: relative;
     color: #D9D9D9;
     font-weight: 400;
@@ -23,16 +23,16 @@ const StyledInput = styled.input`
 
 const StyledIcon = styled.img`
     position: absolute;
-    top: 10px;
+    top: 12px;
     right: 10px;
-    width: 2.2rem;
-    height: 2.2rem;
+    width: 2.2rem!important;
+    height: 2.2rem!important;
 `;
 
 function InputField() {
     return (
         <StyledContainer>
-            <StyledInput placeholder="What are you looking for?" />
+            <StyledInput type="text" placeholder="What are you looking for?" />
             <StyledIcon src={search} alt='magnifying glass' />
         </StyledContainer>
     );
