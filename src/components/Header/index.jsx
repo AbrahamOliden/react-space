de18dd960eccc0c7c7df61c58bc1b6513a75d6df
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 19:05:30 -0600

    style img tags inside StyledHeader

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,9 @@ const StyledHeader = styled.header`
     display: flex;
     justify-content: space-between;
     padding: 60px 24px;
+    img{
+        width: 212px;
+    }
 `;
 
 function Header() {
