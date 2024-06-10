ada0aeffce444cc9159819730bbc0220c44025e8
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 18:45:49 -0600

    create GradientBackground and import GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import styled from "styled-components"
+import GlobalStyle from "./components/GlobalStyles/index.jsx"
+
+const GradientBackground = styled.div`
+background: linear-gradient(175deg, 
+  #041833 4.16%, #04244F 48%, #154580 96.76%);
+  min-height: 100vh;
+`
 
 function App() {
 
   return (
     <>
-      
+      <GradientBackground>
+        <GlobalStyle />
+      </GradientBackground>
     </>
   )
 }
