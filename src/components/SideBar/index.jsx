dc661294a69d09ab1b05f92644624e5b0f92032c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-09 20:41:39 -0600

    create folder for SideBar component

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+function SideBar() {
+    return (
+        <aside>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </aside>
+    );
+};
+
+export default SideBar;
