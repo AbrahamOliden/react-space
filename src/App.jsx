f4ac0efdf9598226780c5ce888d9a955b1c04ec1
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-10 16:54:22 -0600

    standard padding at GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Header from "./components/Header"
 import SideBar from "./components/SideBar"
 
 const GradientBackground = styled.div`
-background: linear-gradient(175deg, 
-  #041833 4.16%, #04244F 48%, #154580 96.76%);
+  background: linear-gradient(175deg, 
+    #041833 4.16%, 
+    #04244F 48%, 
+    #154580 96.76%);
   min-height: 100vh;
+  padding: 60px 24px;
 `
 
 function App() {
