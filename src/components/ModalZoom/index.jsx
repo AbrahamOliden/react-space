8db880b330605069e9893c898387401432c86650
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-11 17:59:41 -0600

    create ModalZoom component

---
+++ src/components/ModalZoom/index.jsx
@@ -0,0 +1,12 @@
+function ModalZoom() {
+    return <>
+        <dialog open>
+            <p>Cierrame we</p>
+            <form method="dialog">
+                <button>Weno</button>
+            </form>
+        </dialog>
+    </>
+};
+
+export default ModalZoom;
