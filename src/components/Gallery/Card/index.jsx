00b735c38f45ae5aa0f9e7d1b6009c2897943ed3
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-10 23:11:15 -0600

    first draft of Card component

--- src/components/Gallery/Card/index.jsx
+++ src/components/Gallery/Card/index.jsx
@@ -1,5 +1,24 @@
-function Image() {
+function Card({ data, fav, expand }) {
 
+    const {titulo, fuente, id, path} = data;
+
+    return (
+        <figure id={id}>
+            <img src={path} alt={titulo} />
+            <figcaption>
+                <h4>{titulo}</h4>
+                <footer>{fuente}</footer>
+                <div>
+                    <button>
+                        <i>{fav}</i>
+                    </button>
+                    <button>
+                        <i>{expand}</i>
+                    </button>
+                </div>
+            </figcaption>
+        </figure>
+    )
 };
 
-export default Image;
+export default Card;
