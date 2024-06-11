f5142b450eee2de02590bd9e51e477d46d36a355
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-11 01:45:23 -0600

    first draft of Card component

--- src/components/Gallery/Card/index.jsx
+++ src/components/Gallery/Card/index.jsx
@@ -1,23 +1,66 @@
-function Card({ data, fav, expand }) {
+import styled from "styled-components";
 
-    const {titulo, fuente, id, path} = data;
+const StyledFigure = styled.figure`
+    width: ${props => props.$expand ? '90%' : '460px'};
+    display: flex;
+    flex-flow: column nowrap;
+    position: relative;
+    & > img {
+        border-radius: 20px 20px 0 0;
+    }
+    figcaption {
+        background-color: #001634;
+        color: #FFF;
+        padding: 16px 24px;
+        border-radius: 0 0 20px 20px;
+        box-sizing: border-box;
+        h3 {
+            font-size: 1.1rem;
+            font-weight: 700;
+            margin: 0 0 8px 0;
+        }
+        h4 {
+            font-size: 1rem;
+            font-weight: 400;
+            margin: 0;
+        }
+        div {
+            position: absolute;
+            display: flex;
+            right: 24px;
+            bottom: 24px;
+            gap: 24px;
+            button {
+                background-color: #001634;
+                border: none;
+                text-align: center;
+                font-size: 1rem;
+                cursor: pointer;
+            }
+        }
+    }
+`;
+
+function Card({ data, favActive, favInnactive, expand }) {
+
+    const {titulo, fuente, path} = data;
 
     return (
-        <figure id={id}>
+        <StyledFigure $expand={false} >
             <img src={path} alt={titulo} />
             <figcaption>
-                <h4>{titulo}</h4>
-                <footer>{fuente}</footer>
+                <h3>{titulo}</h3>
+                <h4>{fuente}</h4>
                 <div>
                     <button>
-                        <i>{fav}</i>
+                        <img src={favActive} />
                     </button>
                     <button>
-                        <i>{expand}</i>
+                        <img src={expand} />
                     </button>
                 </div>
             </figcaption>
-        </figure>
+        </StyledFigure>
     )
 };
 
