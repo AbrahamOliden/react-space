ac76c23a73b694438ac2f449d0e562c97fb3b10b
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-11 17:36:16 -0600

    create IconButton component

---
+++ src/components/IconButton/index.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const IconButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 12px;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+`;
+
+export default IconButton;
