e87236b0c26b505bba735e2958ca2829f3eed2c8
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-10 18:15:18 -0600

    complete first draft

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,24 +1,31 @@
 import styled from "styled-components";
-import banner from '../../assets/banner.png';
 
-const StyledContainer = styled.div`
-    display: inline-block;
-    position: relative;
-    background-image: ${props.$image};
+const StyledContainer = styled.figure`
+    display: flex;
+    background-image: url(${props => props.$image});
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 328px;
+    max-width: 100%;
+    align-items: center;
+    border-radius: 20px;
 `;
 
 const StyledText = styled.h2`
+    max-width: 300px;
     font-size: 2.5rem;
+    font-weight: 400;
+    line-height: 2rem;
     color: #FFF;
-    background: transparent;
-    position: absolute;
-    left: 0;
-    top: 10px;
-    text-align: left;
+    padding: 0 64px;
 `;
 
-function Banner() {};
-    <StyledContainer $image={banner} >
-        <StyledText>The most complete space gallery</StyledText>
+function Banner({ text, backgroundImage }) {
+    return (
+    <StyledContainer $image={backgroundImage}>
+        <StyledText>{ text }</StyledText>
     </StyledContainer>
+    )
+};
+
 export default Banner;
