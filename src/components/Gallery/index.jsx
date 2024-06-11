9ae9ef3763ac1eba6591599159a2a7c1e26e6836
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-10 20:45:47 -0600

    rearrange components

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 import Tag from "./Tags";
 import Title from "../Title";
+import Populars from "./Populars";
+
+const ImagesContainer = styled.div`
+    display: flex;
+`;
+
+const WTFIsHeDoing = styled.section`
+    flex-grow: 1;
+`;
 
 function Gallery() {
     return (
         <>
             <Tag />
-            <Title>Search through the gallery</Title>
+            <ImagesContainer>
+                <WTFIsHeDoing>
+                    <Title>Search through the gallery</Title>
+
+                </WTFIsHeDoing>
+
+                <Populars />
+            </ImagesContainer>
         </>
     );
 };
