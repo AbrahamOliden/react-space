0cc8ef304e2a12774b0172f866d447719553f0d0
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-12 01:52:24 -0600

    add icons and links to my socials

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,26 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faFreeCodeCamp, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const StyledFooter = styled.footer`
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-between;
+    align-items: center;
     padding: 24px 30px;
     background-color: #04244F;
+    color: #FFF;
+    div{
+        display: flex;
+        flex-flow: row nowrap;
+        gap: 30px;
+        font-size: 1.5rem;
+        a {
+            color: inherit;
+            cursor: pointer;
+        }
+    }
     h5 {
-        color: #FFF;
         font-size: 1rem;
         font-weight: 500;
         line-height: 20px;
@@ -17,7 +30,17 @@ const StyledFooter = styled.footer`
 function Footer() {
     return <>
     <StyledFooter>
-        <div></div>
+        <div>
+            <a href="https://github.com/AbrahamOliden" target="_blank">
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="https://www.freecodecamp.org/Abraham_Oliden" target="_blank">
+                <FontAwesomeIcon icon={faFreeCodeCamp} />
+            </a>
+            <a href="https://www.linkedin.com/in/abraham-oliden/" target="_blank">
+                <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+        </div>
         <h5>Developed by Abraham Oliden</h5>
     </StyledFooter>    
     </>
